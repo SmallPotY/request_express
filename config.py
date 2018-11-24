2ded9f5b86105d4cb75a481ed588603bff15a23a
Jie Yang <1041132457@qq.com>
2018-11-24 17:15:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -13,31 +13,31 @@ proxyHost = "http-pro.abuyun.com"
 proxyPort = "9010"
 
 # 代理隧道验证信息
-proxyUser = "HI9OOUUPMH52DN0P"
-proxyPass = "7F153DB0296F039F"
+proxyUser = "**"
+proxyPass = "**"
 
 DB_token = {
     'PGSQL': {
-        'PG_USERNAME': 'smallpot',
-        'PG_PASSWORD': 'yj',
-        'PG_HOST': '58.63.214.44',
-        'PG_PORT': '5432',
+        'PG_USERNAME': '',
+        'PG_PASSWORD': '',
+        'PG_HOST': '',
+        'PG_PORT': '',
         'PG_DATABASE': 'express_spider',
     },
 
     'MSSQL': {
-        'MS_server': '112.94.171.43',
-        'MS_port': '10850',
-        'MS_user': 'sa',
-        'MS_password': '3c-erp',
+        'MS_server': '',
+        'MS_port': '',
+        'MS_user': '',
+        'MS_password': '',
         'MS_database': 'express_spider'
     },
 
     'tiantu': {
-        'MS_server': '120.77.227.163',
-        'MS_port': '1433',
-        'MS_user': 'vip',
-        'MS_password': 'vip2015',
+        'MS_server': '',
+        'MS_port': '',
+        'MS_user': '',
+        'MS_password': '',
         'MS_database': 'Express_Query'
     }
 }
